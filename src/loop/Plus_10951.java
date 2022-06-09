6264c2bfd0c9f923c9bf1462f462c6deb713d756
kang <wonkang96@gmail.com>
2022-06-09 23:56:28 +0900

    10951번 : A+B - 4

---
+++ src/loop/Plus_10951.java
@@ -0,0 +1,21 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Plus_10951 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		
+		while (sc.hasNextInt()) {
+			a = sc.nextInt();
+			b = sc.nextInt();
+			
+			System.out.println(a + b);
+		}
+		sc.close();
+	}
+
+}
