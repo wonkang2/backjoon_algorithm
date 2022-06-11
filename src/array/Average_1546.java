5001083ee9a706d3c921fb8712477b4c94567d4d
kang <wonkang96@gmail.com>
2022-06-11 23:40:27 +0900

    1546번 : 평균

---
+++ src/array/Average_1546.java
@@ -0,0 +1,33 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Average_1546 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int max = 0;
+		int[] scores = new int[n];
+		double average = 0;
+		
+		for(int i = 0; i < n; i++) {
+			scores[i] = sc.nextInt();
+		}
+
+		Arrays.sort(scores);
+		
+		max = scores[n-1];
+
+		for(int i = 0; i < n; i++) {
+			average = average + scores[i] / (double)max * 100;
+		}
+		
+		average = average / n;
+		
+		System.out.println(average);
+	}
+}
