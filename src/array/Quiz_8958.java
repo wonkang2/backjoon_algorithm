0eec10d217cd8d42a0af50c162b4573355a565c6
kang <wonkang96@gmail.com>
2022-06-12 22:50:56 +0900

    8958번 : OX퀴즈

---
+++ src/array/Quiz_8958.java
@@ -0,0 +1,26 @@
+package array;
+
+import java.util.Scanner;
+
+public class Quiz_8958 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			String str = sc.next();
+			int cnt = 0;
+			int total = 0;
+			for(int j = 0; j < str.length(); j++) {
+				cnt ++;
+				if(str.charAt(j) == 'X') {
+					cnt = 0;
+				}
+				total += cnt;
+			}
+			System.out.println(total);
+		}
+		sc.close();
+	}
+}
