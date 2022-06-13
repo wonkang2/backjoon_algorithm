a484caadbb7a9aca225e39d0b67de147a0777357
kang <wonkang96@gmail.com>
2022-06-13 22:27:17 +0900

    15596번 : 정수 N개의 합

---
+++ src/method/N_15596.java
@@ -0,0 +1,24 @@
+package method;
+
+public class N_15596 {
+
+	public static void main(String[] args) {
+	
+		
+	}
+}
+
+class Test {
+
+	int total;
+	
+	long sum(int[] a) {
+		
+		for (int i = 0; i < a.length; i++) {
+			
+			total += a[i];
+		}
+		
+		return total;
+	}
+}
