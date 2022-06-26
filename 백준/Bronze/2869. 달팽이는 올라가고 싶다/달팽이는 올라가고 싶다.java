f957cca2aaa432d7fdab8ccb95e38225effaa8bd
wonkang2 <99650165+wonkang2@users.noreply.github.com>
2022-06-26 19:05:40 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 120 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -12,7 +12,11 @@ public class Main {
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
         int V = Integer.parseInt(st.nextToken());
-        int result = (int)Math.ceil(((V - A) + (A - B)) / (double)(A - B));
+
+        // Math.ceil : 나눴을 때 소숫점이 있으면 아직 이동할 거리가 있다는 것으로 + 1일이 더 필요하다는 의미
+        // (V - B) : 정상에 도착한 후에는 미끄러지지 않으니 (총 높이 - 올라가는 거리)
+        // (A - B) : 미끄러짐까지 포함한 이동 거리
+        int result = (int)Math.ceil((V - B) / (double)(A - B)); 
 
         bw.write(Integer.toString(result));
 
