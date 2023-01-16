4dfa2b92bb37a78294c687c110269a281de427b4
wonkang2 <99650165+wonkang2@users.noreply.github.com>
2023-01-16 10:18:35 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,50 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Stack<String> stack = new Stack<>();
         int T = Integer.parseInt(br.readLine());
-        String[] result = new String[T];
+        StringBuilder sb = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < T; i++) {
-            stack.push(br.readLine());
-        }
-
-        for (int i = 0; i < T; i++) {
-            int checkVPS = 0;
-            char[] chars = stack.pop().toCharArray();
-            if (chars[chars.length - 1] == '(') {
-                checkVPS ++;
-            } else if (chars[0] == ')') {
-                checkVPS --;
-            } else {
-                for (int j = 0; j < chars.length; j++) {
-                    if (chars[j] == '(') {
-                        checkVPS++;
-                    } else {
-                        if (checkVPS < 1) {
-                            checkVPS = -1;
-                            break;
-                        } else {
-                            checkVPS--;
-                        }
+            sb.append(br.readLine());
+            for (int j = 0; j < sb.length(); j++) {
+                char temp = sb.charAt(j);
+                if (temp == '(') {
+                    stack.push(temp);
+                } else { // ')' 일 경우
+                    if (stack.isEmpty()) { // 비어있을 경우 () 성립 x
+                        stack.push(temp);
+                        break;
+                    } else if (stack.peek().equals('(')) {
+                        stack.pop();
                     }
                 }
             }
-            if (checkVPS == 0) {
-                result[result.length - 1 - i] = "YES";
+            if (stack.isEmpty()) {
+                result.append("YES\n");
             } else {
-                result[result.length - 1 - i] = "NO";
+                result.append("NO\n");
             }
+            stack.clear();
+            sb.setLength(0);
         }
-        for (String s : result) {
-            System.out.println(s);
-        }
+        System.out.println(result);
         br.close();
     }
 }
