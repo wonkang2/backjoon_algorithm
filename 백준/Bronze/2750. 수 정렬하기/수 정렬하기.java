75e022296442893073cc7ee628e1e0654d0c3f15
wonkang2 <99650165+wonkang2@users.noreply.github.com>
2023-01-21 11:20:07 +0900

    [Bronze II] Title: 수 정렬하기, Time: 188 ms, Memory: 14880 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,7 +13,15 @@ public class Main {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(arr);
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
 
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
